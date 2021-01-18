0eedf84dcc7cb25a1ac1e53ef11d8d394edd385f
John Esposito <john.esposito@6st.co>
2021-01-18 08:16:03 -0500

    deploy

--- markov/js/Word.js
+++ markov/js/Word.js
@@ -9,8 +9,10 @@ export function Word(wordString, nextWordHopMap) {
         let nextWordStrings = Object.keys(nextWords);
         let nextWordCounts = Object.values(nextWords);
         let nextWordCount = nextWordCounts.reduce((accumulator,currVal) => accumulator + currVal);
+        let denominator = nextWordCount;
         for(let i=0; i<nextWordCount; i++) {
             let candidate = nextWordStrings[i];
+            denominator = nextWordCount - i; //reduce the denominator as we count down -- once a candidate is rejected, it should not be counted as an option
             if(use(candidate)) {
                 nextWord = candidate;
                 break;
@@ -22,7 +24,7 @@ export function Word(wordString, nextWordHopMap) {
             let candidateCount = nextWords[candidate];
             //console.log('candidateCount: ' + candidateCount);
             //console.log('nextWordCount: ' + nextWordCount);
-            let candidateProbability = candidateCount/nextWordCount;
+            let candidateProbability = candidateCount/denominator;
             //console.log('candidateProbability: ' + candidateProbability);
             let random = Math.random();
             //console.log('random: ' + random);

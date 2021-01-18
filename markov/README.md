616517371223f3e95a8817b8ca192f2e69a280e6
John Esposito <john.esposito@6st.co>
2021-01-18 08:04:20 -0500

    deploy

--- markov/README.md
+++ markov/README.md
@@ -1,1 +1,3 @@
 technically a text generator using markov chain?? but the most naive possible atm.
+
+not worth anything, just playing
---
+++ markov/deploy.sh
@@ -0,0 +1,6 @@
+cp -R ./ /Users/johnesposito/OneDrive/Documents/my_website/subwayprophet.github.io/markov
+cd /Users/johnesposito/OneDrive/Documents/my_website/subwayprophet.github.io/markov
+rm -rf .git
+git add .
+git commit -m "deploy"
+git push

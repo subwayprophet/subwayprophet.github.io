535a5f79d7e962e7c0804fe7a27ac72f8b221f36
johnesposito <john.esposito@6st.co>
2023-05-17 07:55:49 -0400

    track high score in localStorage

--- asteroidrage/player.js
+++ asteroidrage/player.js
@@ -1,8 +1,12 @@
+const LOCALSTORAGE_KEY_HIGHSCORE = 'ast_highScore';
+
 export function Player(name) {
     this.name = name;
     this.score = 0;
 
     this.health = 1000;
+    
+    this.highScore = parseInt(localStorage.getItem(LOCALSTORAGE_KEY_HIGHSCORE) || '0');
 
     this.scoreEl;
     this.setScoreEl = function(el) {
@@ -12,14 +16,30 @@ export function Player(name) {
     this.setHealthEl = function(el) {
         this.healthEl = el;
     }
+    this.highScoreEl;
+    this.setHighScoreEl = function(el) {
+        this.highScoreEl = el;
+    }
 
     this.update = function() {
         let p = this;
         p.scoreEl.innerText = p.score;
         p.healthEl.innerText = p.health;
+        p.highScoreEl.innerText = p.highScore;
+        p.checkHighScore();
         window.requestAnimationFrame(function() {
             p.update();
         })
+    }
+
+    this.checkHighScore = function() {
+        let highScoreStr = localStorage.getItem(LOCALSTORAGE_KEY_HIGHSCORE);
+        if(!highScoreStr) highScoreStr = '0';
+        let highScore = parseInt(highScoreStr);
+        if(this.score > highScore) {
+            this.scoreEl.style.color = 'red';
+            localStorage.setItem(LOCALSTORAGE_KEY_HIGHSCORE,this.score);
+        }
     }
 
 }
